0b5a8f0158255971a86af7cc7666330c855f3fe6
Mugsy Carter <mugsycarter@gmail.com>
2017-09-12 18:02:12 -0700

    home controller partially fleshed out.  Portfolio entry templates and sections have been added.

--- app/src/components/home/home.js
+++ app/src/components/home/home.js
@@ -6,4 +6,28 @@ export default {
 };
 
 
-function controller() {};
+function controller() {
+    this.featuredWebsite = {
+        title: 'Carter Hearts',
+        url: 'https://carterhearts.com',
+        ImageURL: './images/carterHearts.png',
+        description: 'A web-based single-player version of the card game hearts.',
+        takeaway: 'I spend a lot of time cpntinually tweaking the AI to make it smarter.  Eventually I got it to place where it could win games and even shoot the moon.  I even ended up adding "easy" AI players as a result of getting feedback from people that it was too difficult.  Give it a try!',
+        date: 'March 2017',
+        tech: 'MEAN Stack'
+    };
+
+    this.soloProjects = [
+        {
+            title: 'Carter Hearts',
+            url: 'https://carterhearts.com',
+            ImageURL: './images/carterHearts.png',
+            description: 'A web-based single-player version of the card game hearts.',
+            takeaway: 'I spend a lot of time cpntinually tweaking the AI to make it smarter.  Eventually I got it to place where it could win games and even shoot the moon.  I even ended up adding "easy" AI players as a result of getting feedback from people that it was too difficult.  Give it a try!',
+            date: 'March 2017',
+            tech: 'MEAN Stack'
+        }
+    ];
+    this.groupProjects = [];
+
+};
