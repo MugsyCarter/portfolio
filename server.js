93aff70b1a0030c72864da1c0b34d37325a618c7
Mugsy Carter <mugsycarter@gmail.com>
2017-09-13 10:26:41 -0700

    one picture added and pictures displaying

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use(express.static(pubDir));
 
   // Initialize the app.
 var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log('App now running on port', port);
+    const port = server.address().port;
+    console.log('App now running on port', port);
 });
