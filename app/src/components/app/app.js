cafaaba8dc4b596e1f6be6e016522f6dd3a70227
Mugsy Carter <mugsycarter@gmail.com>
2017-10-04 14:19:16 -0700

    home panel styling updated

--- app/src/components/app/app.js
+++ app/src/components/app/app.js
@@ -8,26 +8,12 @@ export default {
 
 function controller() {
 
-//this code prevents scrolldown on an iphone
-    var xStart, yStart = 0;
- 
-    document.addEventListener('touchstart',function(e) {
-        xStart = e.touches[0].screenX;
-        yStart = e.touches[0].screenY;
-    });
-    
-    document.addEventListener('touchmove',function(e) {
-        var xMovement = Math.abs(e.touches[0].screenX - xStart);
-        var yMovement = Math.abs(e.touches[0].screenY - yStart);
-        if((yMovement * 3) > xMovement) {
-            e.preventDefault();
-        }
-    });
-
     this.link = {
         home: true,
-        test: false,
-        about: false
+        projects: false,
+        bio: false,
+        contact: false,
+        resume: false
     };
 
     this.clicked = (page)=>{
@@ -35,8 +21,10 @@ function controller() {
             console.log('updating link');
             this.link = {
                 home: false,
-                test: false,
-                about: false
+                projects: false,
+                bio: false,
+                contact: false,
+                resume: false
             };
             this.link[page] = true;
         }
