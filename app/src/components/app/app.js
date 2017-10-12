a20f3c5bd0e3317d01c731f13b5e291f4355bec3
Mugsy Carter <mugsycarter@gmail.com>
2017-10-12 09:26:29 -0700

    more styling updates

--- app/src/components/app/app.js
+++ app/src/components/app/app.js
@@ -16,6 +16,8 @@ function controller() {
         resume: false
     };
 
+    this.header = false;
+
     this.clicked = (page)=>{
         if (this.link[page] === false){
             console.log('updating link');
@@ -27,6 +29,12 @@ function controller() {
                 resume: false
             };
             this.link[page] = true;
+        }
+        if (this.link.home === true){
+            this.header = false;
+        }
+        else{
+            this.header = true;
         }
     };
 };
